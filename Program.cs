d48e0d9a524ccd07105dac7f4b5a9d31ee96eaad
ViniciusMirandaDev <ViniciusMirandaDev>
2020-06-18 15:06:22 -0300

    Projeto Inicado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula20_Sprint4_Fixacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
